7ad6c48f30162d7b8d3226d88f7faa1ebda39b3f
= <b322029@iiit-bh.ac.in>
2023-08-29 00:05:36 +0530

    no msg

--- test.cxx
+++ test.cxx
@@ -1,8 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-
-cout<<"Hello Bhadwey";
+    int t,x,y;
+    cin>>t;
+    while(t>=1)
+    {
+       cin>>x>>y;
+       if(x+y>6)
+       {
+        cout<<"YES";
+       }
+       else
+       cout<<"NO";
+       t--;
+    }
 
 
 return 0;
